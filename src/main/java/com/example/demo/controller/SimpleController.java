47cfcf85736c23261c0feb81f77ac6e17b794d5e
umeshreddy <umeshreddymr@gmail.com>
2020-10-19 20:04:16 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SimpleController {
+
+	
+	@GetMapping("hello")
+	public String hello() {
+
+		System.out.println("hello...................");
+		return "Your time starts now...";
+	}
+}
